51ebae3efa4c0254bc7cbccd4f54e50005da54fa
ikuruzum <105636211+ikuruzum@users.noreply.github.com>
2023-04-03 11:29:50 +0300

    made pub global addable

---
+++ bin/kaan_json_to_dart.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:kaan_json_to_dart/helpers.dart';
+import 'package:kaan_json_to_dart/model_generator.dart';
+import 'package:path/path.dart';
+
+void main(List<String> arguments) {
+  var ayirici = Platform.isWindows ? "\\" : "/";
+
+  stdout.writeln('Json nerede (/x/y/z/abc.json)?');
+  final yer = stdin.readLineSync() ?? "";
+
+  if (!Directory(yer).existsSync() && !File(yer).existsSync()) {
+    stdout.writeln('Geçerli yer girmelisin');
+    exit(1);
+  }
+
+  if (Directory(yer).existsSync()) {
+    Directory(yer).listSync().forEach((entity) {
+      if (entity is File) {
+        var ad = basename(yer);
+        tekDosya(ad, yer, ayirici, true);
+      }
+    });
+  } else if (File(yer).existsSync()) {
+    stdout.writeln('Adı ne olsun?');
+    final ad = stdin.readLineSync() ?? "";
+    tekDosya(camelCase(ad), yer, ayirici, true);
+  }
+}
+
+String reversed(String a) {
+  return a.split('').reversed.join();
+}
+
+String toSnakeCase(String val) {
+  RegExp exp = RegExp(r'(?<=[a-z])[A-Z]');
+  return val
+      .replaceAllMapped(exp, (Match m) => ('_${m.group(0) ?? ""}'))
+      .toLowerCase();
+}
+
+void tekDosya(String ad, String yer, String ayirici, [modelfile = false]) {
+  try {
+    final classGenerator = ModelGenerator(ad);
+    final jsonRawData = File(yer).readAsStringSync();
+    DartCode dartCode = classGenerator.generateDartClasses(jsonRawData);
+    var yazilcak = reversed(yer);
+    var ek = "";
+    if (modelfile) {
+      ek = "model$ayirici";
+    }
+    yazilcak = yazilcak.substring(yazilcak.indexOf(ayirici));
+    yazilcak = reversed(yazilcak);
+    var dosya = File("$yazilcak$ek${toSnakeCase(ad)}.dart");
+    if (dosya.existsSync()) {
+      stdout.writeln(
+          "${basename(dosya.path)} diye bir dosya mevcut, bu es geçiliyor. Geçilmesini istemiyorsan dosyayı sil");
+    }
+    dosya.createSync();
+    dosya.writeAsStringSync(dartCode.code);
+  } catch (e) {
+    stdout.writeln(
+        "bir şeyler yanlış gitti, sorunu bulabilmen için ilgili hata ise şu : \n\n\n  $e");
+  }
+}
